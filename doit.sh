8ffba86c5b345027249b4d7a6eeb69daf056726d
David Pritchard <davidp@mentalcanvas.com>
2020-03-21 11:01:24 -0400

    - Recommit, Mar. 20th data and a few script cleanups

--- doit.sh
+++ doit.sh
@@ -1,1 +1,9 @@
-cat list.txt | sed 's#.*#wget -O "&" https://aces.kflaphi.ca/aces/ILIMapper/getPercentages?PHU=&#'
+#!/bin/sh
+export PHUs="APH BCHU DRHD GBHU HNHU HKPRDHU HRHD CHPHS HPE HCHU CKPHS KFLA
+    CLCHSD LGL MLHU NPHU NBPHU NWHU COHU PEEL PDHU PCCHU PHU RCDHU EOHU SMDHU
+    SWPH SDHU TBPHU THU WHU WDGHU WECHU YRHU TOR"
+for PHU in $PHUs; do
+    wget -O - https://aces.kflaphi.ca/aces/ILIMapper/getPercentages?PHU=$PHU \
+        | json_pp \
+        > $PHU.json
+done
